3124db420002cbca0619a59a76107ce68bee4b4a
akshi-v <133794768+akshi-v@users.noreply.github.com>
2023-07-19 12:52:46 +0530

    Update Diamond_streamlit_app.py

--- Diamond_streamlit_app.py
+++ Diamond_streamlit_app.py
@@ -11,68 +11,72 @@ import numpy as np
 
 # Load the model from the pickle file
 model = pickle.load(open('diamond_pred.pkl', 'rb'))
+# Define the unique values for 'cut', 'color', and 'clarity'
+cut_values = ['Ideal', 'Premium', 'Good', 'Very Good', 'Fair']
+color_values = ['E', 'I', 'J', 'H', 'F', 'G', 'D']
+clarity_values = ['SI2', 'SI1', 'VS1', 'VS2', 'VVS2', 'VVS1', 'I1', 'IF']
 
-# Define categorical columns for one-hot encoding
-categorical_cols = ['cut', 'color', 'clarity']
+# Function to perform one-hot encoding
+def one_hot_encode(df, column, values):
+    for value in values:
+        df[column + '_' + value] = (df[column] == value).astype(int)
+    df.drop(column, axis=1, inplace=True)
 
-# Define numerical columns for scaling
-numerical_cols = ['carat', 'depth', 'table', 'x', 'y', 'z']
+# Function to get user input for prediction
+def get_user_input():
+    carat = st.slider('Carat', 0.2, 5.01, 0.8)
+    cut = st.selectbox('Cut', cut_values)
+    color = st.selectbox('Color', color_values)
+    clarity = st.selectbox('Clarity', clarity_values)
+    depth = st.slider('Depth', 43, 79, 61)
+    table = st.slider('Table', 43, 95, 57)
+    x = st.slider('x', 0, 10, 5)
+    y = st.slider('y', 0, 60, 30)
+    z = st.slider('z', 0, 32, 16)
+    
+    # Create a dictionary with the user input
+    data = {
+        'carat': carat,
+        'cut': cut,
+        'color': color,
+        'clarity': clarity,
+        'depth': depth,
+        'table': table,
+        'x': x,
+        'y': y,
+        'z': z
+    }
+    
+    # Convert the dictionary to a DataFrame
+    features = pd.DataFrame(data, index=[0])
+    
+    # Perform one-hot encoding with remove_first option for specific features
+    one_hot_encode(features, 'cut', cut_values)
+    one_hot_encode(features, 'color', color_values)
+    one_hot_encode(features, 'clarity', clarity_values)
+    
+    # Reorder the columns to match the order used during model training
+    features = features[model.feature_names_in_]
+    
+    return features
 
-# Create a DataFrame with default values for user input
-default_data = {
-    'carat': 0.5,
-    'depth': 60.0,
-    'table': 55.0,
-    'cut': 'Ideal',
-    'color': 'D',
-    'clarity': 'IF',
-    'x': 5.0,
-    'y': 5.0,
-    'z': 3.0
-}
-input_df = pd.DataFrame(default_data, index=[0])
+# Set the title of the app
+st.title('Diamond Price Prediction App')
 
-#
+# Get user input
+user_input = get_user_input()
 
+# Make prediction
+prediction = model.predict(user_input)
+predicted_price = round(prediction[0], 2)
 
+# Display the prediction
+st.subheader('Prediction')
+st.write('The predicted price of the diamond is $', predicted_price)
 
-# Streamlit app
-st.title("Diamond Price Prediction")
-st.write("Enter the diamond features below to predict the price:")
-
-# Create input fields for each feature
-carat = st.number_input("Carat", min_value=0.0, value=0.5)
-depth = st.number_input("Depth", min_value=0.0, value=60.0)
-table = st.number_input("Table", min_value=0.0, value=55.0)
-cut = st.selectbox("Cut", options=['Ideal', 'Premium', 'Very Good', 'Good', 'Fair'])
-color = st.selectbox("Color", options=['D', 'E', 'F', 'G', 'H', 'I', 'J'])
-clarity = st.selectbox("Clarity", options=['IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1'])
-x = st.number_input("x", min_value=0.0, value=5.0)
-y = st.number_input("y", min_value=0.0, value=5.0)
-z = st.number_input("z", min_value=0.0, value=3.0)
-
-# Update the input DataFrame with user values
-input_df = pd.DataFrame({
-    'carat': carat,
-    'depth': depth,
-    'table': table,
-    'cut': cut,
-    'color': color,
-    'clarity': clarity,
-    'x': x,
-    'y': y,
-    'z': z
-}, index=[0])
-
-
-# Predict the price based on user input
-predicted_price = model.predict(input_df)
-
-st.write(f"Predicted Price: {predicted_price}")
-
-
-# In[ ]:
-
+# Additional information and data display
+st.subheader('User Input Features')
+st.write(user_input)
 
 
 
